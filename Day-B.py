3ec1606a38525112e0041baf7f70f81b44af2f4b
Vaidehi Doke <142891413+Vaidu7972@users.noreply.github.com>
2026-02-02 17:16:35 +0530

    Add files via upload

---
+++ Day-B.py
@@ -0,0 +1,93 @@
+ # FUNCTIONS 
+
+"""Block / Group of code that performs a specific task.
+reusable code.
+avoid rewriting the same code again and again.(reduce code redundancy)
+(DRY- Don't Repeat Yourself)
+
+Two types of functions:   
+
+1. Built-in functions: 
+   pre-defined functions in python
+   e.g.- print(), input(), len(), type(), range(), int(), str() etc
+
+2. User-defined functions: 
+   functions defined by the user to perform specific tasks.
+  
+
+Syntax:
+def function_name(parameters / inputs):
+keyword 'def' is used to define a function.
+"""
+
+a=10
+b=20
+c=a+b
+print("Sum is:",c)
+print(c,"\n")
+
+#instead of writing above code again and again we can use function
+
+def add_numbers(x, y):             #function definition  #a,b are parameters 
+    c = x + y
+    print(c,"\n")
+    
+add_numbers(10,20)                    #function call  #10,20 are arguments
+add_numbers(100,200)                  #function call  
+
+#or
+
+def add_numbers(x, y):
+    return x + y                      #fuction definition 
+
+result = add_numbers(10, 20)         #function call
+print(result,"\n")
+
+
+#  1. Fuction with arguments and return Type 
+
+def square(num):                    #fuction call
+    return num * num
+
+result = square(5)
+print(result,"\n")
+
+#  2. function with no arguments and no return type
+
+def greet():
+    print("Hello, Welcome to Python Functions!\n")
+
+greet()                             #function call
+greet()                             #function call
+
+#  3. function with arguments and no return type
+
+def greet(name):
+    print("Hello", name, "Welcome to Python Functions!\n")  
+
+greet("vaidehi")                     #function call
+
+# 4. function with no arguments and return type
+
+def greeting():
+    return "Hello, Welcome to Python Functions!\n"
+
+message = greeting()                 #function call
+print(message)
+
+# EXAMPLE : avg of 3 numbers
+
+def average(a,b,c):
+    avg = (a+b+c)/3
+    print(avg)
+    
+    avg(10,20,30)       #function call
+    
+#  by default arguments    
+
+def power(a,b=2):
+    result = a ** b
+    print(result)
+
+power(5)                             # here b takes default value 2
+power(2,3)                           # here b takes value 3,overrides on default value
