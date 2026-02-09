c0143140a0bb712468ec91f9c2b0f1dc151a796e
Vaidehi Doke <142891413+Vaidu7972@users.noreply.github.com>
2026-02-09 20:09:29 +0530

    Add files via upload

---
+++ Day-G.py
@@ -0,0 +1,115 @@
+# OOP : Object Oriented Programing
+
+# OOP 4 pillors: Abstraction 
+#                Encapsulation
+#                Inheritance
+#                Polymorphism
+
+
+# Abstraction:
+
+"""
+Hiding the implementation details of a class
+and showing only essential features to the user.
+Example: Car (we know how break works but dont knoe their exact function/mechanism.)
+"""
+
+class Car:
+    def __init__(self):
+        self.acc = False
+        self.brk = False
+        self.clutch = False
+        
+    def start(self):
+        self.clutch = True
+        self.acc = True
+    print("Car started...")
+    
+# Object creation (outside the class)
+car1 = Car()
+car1.start()
+print("\n")
+
+# Encapsulation  
+
+"""
+Data + Function = Capsule  (single unit called object)
+"""
+
+class Car:   
+    def __init__(self):
+        self.speed = 0                # private variable
+
+    def accelerate(self):
+        self.speed = 60
+        print("Car is moving")
+
+    def show_speed(self):
+        print("Speed:", self.speed)
+
+car1 = Car()
+car1.accelerate()
+car1.show_speed()
+print("\n")
+
+# del Keyword
+"""
+used to delete object proprties or object itself
+Syntax : del s1.name
+         del s1
+"""   
+
+class Student:
+    def __init__(self, name):
+        self.name = name
+
+s1 = Student("Vaidehi")                             # Creating object (outside the class)
+print(s1.name,"\n")                                      # Printing object data
+
+del s1                                              # Deleting object
+
+# print(s1.name)                                      # Gives error
+
+
+
+#function inside a class. 
+
+class Account:                               #Class
+    def __init__(self,accno,accpass):        #Constructor
+        self.accno=accno                     #Instance Variables
+        self.accpass=accpass
+
+    def resetpass(self):                     #Method :function inside a class.                  
+        print(self.accpass)
+
+acc1=Account("123","abc")                    #Object
+print(acc1.accno)
+print(acc1.accpass)
+print(acc1.resetpass)
+
+        
+# Private (like) Attribute & Method
+"""
+used only within class and  not accessable from outside the class (private)like above ex.
+private members are created using double underscore __ before the name.
+They are not accessible directly from outside the class.
+"""
+
+class Person:
+    __name="Giri"              #__name is a private attribute.
+
+    def __hello(self):         #__hello() is a private method.
+        print("hello")
+
+    def welcome(self):         #welcome() is a public method. It accesses the private method inside the class. This is the correct way to use private members.
+        self.__hello()
+
+p1=Person()
+print(p1.welcome())            #print : none  #Encapsulation 
+
+"""
+p1.welcome() calls the private method internally.
+welcome() prints "hello".
+Since welcome() does not return anything, it returns None.
+print() displays None.
+"""  
